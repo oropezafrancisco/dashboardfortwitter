9ddd42256294f6edd16e087db2a1d56e35fe2a29
Francisco Oropeza <45477020+oropezafrancisco@users.noreply.github.com>
2018-12-05 13:21:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var spawn = require('child_process').exec;
 
 spawn('twitter-proxy');
-spawn('http-server');
+spawn('http-server -P http://localhost:7890');
 
 console.log('Server running on http://localhost:8080');
 console.log('Request the Twitter API using: http://localhost:7890/1.1/statuses/user_timeline.json\?count\=30\&screen_name\=makeschool');
